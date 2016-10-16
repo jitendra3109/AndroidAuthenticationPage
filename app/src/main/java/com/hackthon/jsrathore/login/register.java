e29e58226c8991769472442b062404add7c56024
jsroyal <jitu55iiit@gmail.com>
2016-10-16 17:26:30 +0530

    connect with each other login and register

--- app/src/main/java/com/hackthon/jsrathore/login/register.java
+++ app/src/main/java/com/hackthon/jsrathore/login/register.java
@@ -1,13 +1,29 @@
 package com.hackthon.jsrathore.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
-
+   private Button registerMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+       registerMe=(Button)findViewById(R.id.register);
+
+
+     registerMe.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             startActivity(new Intent(register.this,login.class));
+
+         }
+
+         });
+
     }
 }
